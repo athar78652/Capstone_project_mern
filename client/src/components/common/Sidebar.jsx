445a475c2a7418892f0c669cf0f47d3e5d0e9c9e
athar <athar78652@gmail.com>
2023-03-04 20:19:20 +0530

    authmodal created

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -1,9 +1,9 @@
 import { Drawer, List, ListItemButton, ListItemIcon, ListItemText, Stack, Toolbar, Typography } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import menuConfigs from "../../configs/menu.config";
+import menuConfigs from "../../configs/menu.configs";
 import Logo from "./Logo";
-import uiConfigs from "../../configs/ui.config";
+import uiConfigs from "../../configs/ui.configs";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
 
