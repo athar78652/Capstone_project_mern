c0c29c075abd269f4f9a282999ef1bb1c800c3c3
Shivam Chaudhary <shivamchaudhary310899@gmail.com>
2023-03-05 18:00:23 +0530

    theme config

--- client/src/configs/theme.configs.js
+++ client/src/configs/theme.configs.js
@@ -10,7 +10,7 @@ const themeConfigs = {
   custom: ({ mode }) => {
     const customPalette = mode === themeModes.dark ? {
       primary: {
-        main: "#ff0000",
+        main: "#22404A",
         contrastText: "#ffffff"
       },
       secondary: {
